559bdc977330edf3a88cbbfea84d5c940727ffea
aresous <aresous@gmail.com>
2022-04-11 22:10:26 -0400

    styling, edit, delete buttons

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,7 @@
+import { findByLabelText } from "@testing-library/react";
+import { hover } from "@testing-library/user-event/dist/hover";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function PostCard(props) {
   const post = props.post;
@@ -34,7 +37,9 @@ export default function PostCard(props) {
   }
   const theStyle = {
     content: "",
-    backgroundImage: 'url("https://i.imgur.com/nhX3VEW.jpeg")',
+    display: "flex",
+    justifyContent: "space-between",
+    backgroundImage: `url(${post.imgURL})`,
     backgroundSize: "cover",
     position: "absolute",
     top: "0px",
@@ -51,31 +56,65 @@ export default function PostCard(props) {
         // backgroundImage: `url("https://i.imgur.com/nhX3VEW.jpeg")`,
         // opacity: "0.5",
         position: "relative",
+        display: "flex",
+        justifyContent: "space-around",
       }}
     >
       <div style={theStyle}></div>
-      <p style={{ display: "inline", position: "relative" }}>Publish : </p>
-      <label className="switch">
-        <input
-          type="checkbox"
-          defaultChecked={checked}
-          onChange={() => updatePublished()}
-        />
-        <span className="slider round"></span>
-      </label>
-      <span style={{ position: "relative" }}>
-        {post.title}
-        {post.content}
+      {/* <p style={{ display: "inline", position: "relative" }}>Publish : </p> */}
+      <div style={{ position: "relative" }}>
+        Publish:
+        <label className="switch">
+          <input
+            type="checkbox"
+            defaultChecked={checked}
+            onChange={() => updatePublished()}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <span style={{ position: "relative" }}>{post.title}</span>
 
+      <div>
         <button
           onClick={(e) => {
             deletePost();
-            console.log("button pressed");
+          }}
+          style={{
+            position: "relative",
+            height: "fill",
+            backgroundColor: "red",
+            textDecoration: "none",
+            border: "none",
+            height: "30px",
+            border: "solid 1px white",
+            cursor: "pointer",
+            color: "white",
+            borderRadius: "5px",
+            marginRight: "20px",
           }}
         >
-          X
+          DELETE
         </button>
-      </span>
+        <Link
+          to={`/BlogManagement/${post._id}`}
+          style={{
+            position: "relative",
+            height: "fill",
+            backgroundColor: "green",
+            textDecoration: "none",
+            border: "none",
+            height: "30px",
+            border: "solid 1px white",
+            cursor: "pointer",
+            color: "white",
+            borderRadius: "5px",
+            padding: "5px",
+          }}
+        >
+          EDIT
+        </Link>
+      </div>
     </div>
   );
 }
