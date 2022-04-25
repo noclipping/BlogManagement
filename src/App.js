642933070510f82db8fc0b8076d46f56a4157482
aresous <aresous@gmail.com>
2022-04-25 02:17:03 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Nav from "./Nav";
-import CreatePost from "./CreatePost";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ManagePosts from "./ManagePosts";
-import UpdatePost from "./components/UpdatePost";
+import logo from './logo.svg'
+import './App.css'
+import Nav from './Nav'
+import CreatePost from './CreatePost'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import ManagePosts from './ManagePosts'
+import UpdatePost from './components/UpdatePost'
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route exact path="/newpost" element={<CreatePost />} />
-          <Route exact path="/manage-posts" element={<ManagePosts />} />
-          <Route exact path="/:id" element={<UpdatePost />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Routes>
+                    <Route exact path="/" element={<CreatePost />} />
+                    <Route
+                        exact
+                        path="/manage-posts"
+                        element={<ManagePosts />}
+                    />
+                    <Route exact path="/:id" element={<UpdatePost />} />
+                    <Route
+                        path="*"
+                        element={
+                            <main style={{ padding: '1rem' }}>
+                                <p>There's nothing here!</p>
+                            </main>
+                        }
+                    />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
