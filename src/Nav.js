cfdca70c20717675f9bc5b553dc22cc45d532c2c
aresous <aresous@gmail.com>
2022-04-25 02:45:30 -0400

    fixed route

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Nav() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/newpost"> Create Post</Link>
-        </li>
-        <li>
-          <Link to="/manage-posts">Manage Posts</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Create Post</Link>
+                </li>
+                <li>
+                    <Link to="/manage-posts">Manage Posts</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
