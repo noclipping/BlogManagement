282d1460981e585d99805a3234488fe04c38b75e
aresous <aresous@gmail.com>
2022-04-26 00:05:36 -0400

    updated constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-// export const domain = 'https://frozen-ravine-86869.herokuapp.com/'
+export const domain = 'https://frozen-ravine-86869.herokuapp.com/'
 
-export const domain = 'http://localhost:3005/'
+// export const domain = 'http://localhost:3005/'
